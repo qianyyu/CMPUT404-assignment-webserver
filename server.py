7050cc0ff96d1c4a41e52125e01a840441457689
Qian Yu <529823104@qq.com>
2020-01-23 14:04:36 -0700

    jan 23

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socketserver
 from pathlib import Path
 import os
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Qian Yu
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,56 +27,67 @@ import os
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-'''
-        parser['method'] = method
-        parser['sourcePath'] = sourcePath
 
-'''
 # https://stackoverflow.com/questions/2052390/manually-raising-throwing-an-exception-in-python
 
 
 BUFFER_SIZE = 1024
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
     def handle(self):
         self.data = self.request.recv(BUFFER_SIZE).strip()
+        if not self.data:
+            return
+
         self.parser = {}
-        self.avail_methods = {
+        self.request_methods = {
             "GET": self.do_get
         }
 
         requests = self.data.decode().split('\r\n')
+        #for request in requests:
+            #print(request)
 
+        self.method = requests[0].split(' ')[0]
+        self.source_path = requests[0].split(' ')[1]
+        self.parser['method'] = self.method
+        self.parser['source_path'] = 'www'+self.source_path   
 
-        for request in requests:
-            print(request)
-        
-
-        method = requests[0].split(' ')[0]
-        self.parser['method'] = method
-        sourcePath = requests[0].split(' ')[1]
-        self.parser['sourcePath'] = './www'+sourcePath
-
-        if method in self.avail_methods:
-            if Path(self.parser['sourcePath']).is_dir():
-                if(self.parser['sourcePath'][-1]=='/'):
-                    res = self.avail_methods[method]()
-                else:
-                    res = self.return_header('301',[sourcePath])
-            else:
-                res = self.avail_methods[method]()
-        else:
-            res = self.return_header('405')
-
-        # print ("Got a request of: %s\n" % self.data)
+        res = self.header_handler()
         self.request.sendall(res.encode(encoding='UTF-8'))
 
+    def header_handler(self):
+        method = self.parser['method']
+        if(method != 'GET'):
+            return self.return_header('405')
+        if(not self.validation(self.parser['source_path'])):
+            return self.return_header('404')
 
+        if Path(self.parser['source_path']).is_dir():
+            if(self.parser['source_path'][-1]=='/'):
+                res = self.request_methods[method]()
+            else:
+                res = self.return_header('301',[self.source_path])
+        else:
+            res = self.request_methods[method]()
+        return res
+
+
+    def validation(self,source_path):
+        if('..' in source_path.split('/') or '.' in source_path.split('/')):
+            print('hahah')
+            return False
+            # raise Exception('Only files in ./www and deeper will be served.')
+        return True
+
+    # =============================== Get Method ===============================
     def do_get(self):
         try:
-            path = self.parser['sourcePath'] + ('index.html' if self.parser['sourcePath'][-1] == '/' else '') 
-
+            path = self.parser['source_path'] + ('index.html' if self.parser['source_path'][-1] == '/' else '') 
+            self.validation(self.parser['source_path'])
             f = open(path,'r')
+
             content = ''
             while True:
                 text = f.read(BUFFER_SIZE)
@@ -87,47 +98,56 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
             file_type = path.split('.')[-1]
 
+
             content_type_dic = {
                 'html': 'Content-Type: text/html\r\n',
                 'css' : 'Content-Type: text/css\r\n'
             }
             content_type = content_type_dic[file_type]
-            print(path)
-            print(content_type)
-            print('\n\n\n')
-
             return self.return_header('200',[content_type,content])
-        except Exception as e:
-            #if e == FileNotFoundError:
-            return self.return_header('404')
-            print(e)
 
+        except FileNotFoundError as e:
+            return self.return_header('404')
+    # =============================== Get Method ===============================  
+
+
+    # =============================== HTTP RESPONSE STATUS CODES ===============================
 
     def return_header(self,status_code,args=[]):
-        if(status_code == '200'):
-            return self.ok(args)
-        if(status_code == '301'):
-            return self.move_permanently(args)
-        if(status_code == '404'):
-            return 'HTTP/1.1 404 Not Found\r\n'
-        if(status_code == '405'):
-            return 'HTTP/1.1 405 Method Not Allowed\r\n'
+        response_status_code = {
+            '200': self.ok,
+            '301': self.move_permanently,
+            '404': self.not_found,
+            '405': self.method_not_allowed
+        }
+        return response_status_code[status_code](args)
+
+    def ok(self,args):
+        header = 'HTTP/1.1 200 OK\r\n'
+        content_length = 'Content-Length: ' + str(len(args[1])) + '\r\n'
+        content_type = args[0]
+        content = args[1]
+        response = header+content_length+content_type+'\r\n'+content
+        return response
 
     def move_permanently(self,args):
         header = 'HTTP/1.1 301 Moved Permanently\r\n'
         new_location = 'Location: '+args[0]+'/\r\n'
-        res = header+new_location
+        response = header+new_location
         #print(res)
-        return res
+        return response
 
-    def ok(self,args):
-        header = 'HTTP/1.1 200 OK\r\n'
-        contentLen = 'Content-Length: ' + str(len(args[1])) + '\r\n'
-        res = header+contentLen+args[0]+'\r\n'+args[1]
-        #print(res)
-        return res
+    def not_found(self,args):
+        header = 'HTTP/1.1 404 Not Found\r\n'
+        response = header
+        return response
 
+    def method_not_allowed(self,args):
+        header = 'HTTP/1.1 405 Method Not Allowed\r\n'
+        response = header
+        return response
 
+    # =============================== HTTP RESPONSE STATUS CODES ===============================
 
 
 
